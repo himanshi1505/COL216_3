15a58a32d48f719457a8c44f953409faac0e1207
himanshi1505 <148051546+himanshi1505@users.noreply.github.com>
2025-04-27 23:34:09 +0530

    Create bus.cpp

---
+++ bus.cpp
@@ -0,0 +1,26 @@
+#include "bus.h"
+
+BusTransaction::BusTransaction(BusTransactionType t, uint32_t a, int s, int c) 
+    : type(t), addr(a), source_core(s), transfer_cycles_left(c) {}
+
+bool Bus::busy() const { 
+    return current.type != NONE; 
+}
+
+void Bus::start(BusTransactionType type, uint32_t addr, int core, int cycles) {
+    current = BusTransaction(type, addr, core, cycles);
+}
+
+void Bus::tick() {
+    if (current.type != NONE) {
+        current.transfer_cycles_left--;
+        if (current.transfer_cycles_left <= 0) {
+            current.type = NONE;
+        }
+    }
+}
+
+int Bus::transfer_cycle_left()
+{
+    return current.transfer_cycles_left;
+}
